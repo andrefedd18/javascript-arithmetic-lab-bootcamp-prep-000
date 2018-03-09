5f60f9c46ac71ea99766ee86738cffec14446ba8
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-09 03:50:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ function dec(a){ //Decrement a by 1 and return the result.
   number = a - 1;
   return number;
 }
-function makeInt(a){ //Parse a as an integer and return that integer.
+function makeInt(a){
   number = parseInt(a, 10);
   return number;
 }
-function preserveDecimal(a){ //Parse a as a float (number with decimals) and return that number.
+function preserveDecimal(a){ 
   number = parseFloat(a, 10);
   return number;
 }
